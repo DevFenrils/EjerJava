01c55dfb587711a465eb1f42f2e1bd9e397e096d
pujolman <jlopezgil1991@gmail.com>
2018-05-26 19:21:29 +0200

    second java

---
+++ JavaApplication1/src/javaapplication1/JavaApplication2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Jorge
+ */
+public class JavaApplication2 {
+
+    public Persona personaObj1;
+    public Persona personaObj2;
+    public Persona personaObj3;
+    
+    public void main(String[] args) {
+        // TODO code application logic here
+        
+        String nombre = "Jorge";
+        Integer edad = 27;
+        char sexo = 'H';
+        Double peso = 56.5;
+        Double altura = 1.67;
+        
+                
+        
+        
+    }
+    
+}
